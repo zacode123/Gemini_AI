f9d9fd7ff43071801d4f0cb2763e716bcdeffe58
zacode123 <zuliabegum0@gmail.com>
2025-06-16 14:57:44 +0530

    Add files via upload

---
+++ Examples/Gemini_Ask_From_Serial/Gemini_Ask_From_Serial.ino
@@ -0,0 +1,30 @@
+MIT License
+
+Copyright (c) 2025 Zahid Arman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+#include <Gemini_AI.h>
+#include <credentials.h
+
+// Create an instance of the Gemini_AI class
+Gemini_AI gemini(ssid, password, model, token, maxTokens, systemInstruction, temperature, TopP, TopK, codeExecution, googleSearch, ledmode);
+
+void setup() {
+  Serial.begin(115200);
+ if (gemini.connectToWiFi()) {  // DO NOT REMOVE! library's connect to wifi function
+  Serial.println("AI assistant is ready!");
+  Serial.print(">> ");
+ } else {
+  ESP.restart();
+ }
+}
+
+void loop() {
+  gemini.loop(); // for serial listening and running important functions
+}
