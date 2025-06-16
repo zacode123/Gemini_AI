b2375d2299066ed57c1c0870305fec0d5f9fb6c2
zacode123 <zuliabegum0@gmail.com>
2025-06-16 15:18:50 +0530

    Update Gemini_AI.h

--- src/Gemini_AI.h
+++ src/Gemini_AI.h
@@ -1,3 +1,5 @@
+/*
+
 MIT License
 
 Copyright (c) 2025 zacode123
@@ -8,6 +10,8 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
+*/
 
 #pragma once
 
